cb7496917cf15af11d1e4c899c4b0e7bd4c4bd1d
@tyler-lady <tclady53@gmail.com>
2020-08-10 18:01:23 -0400

    V1

--- server/models/review.js
+++ server/models/review.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
         },
         rating: {
-            type: DataTypes.INTEGER,
+            type: DataTypes.STRING,
             allowNull: false,
         },
         description: {
